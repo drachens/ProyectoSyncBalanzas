1d6d2648a87ff3f9a1adc5bc8e9d32f91a380121
drachens <drachen_665@hotmail.com>
2025-05-23 15:44:50 -0400

    [22/05/2025] refactor: Se eliminan dependencias obsoletas

--- src/main/java/com/marsol/sync/domain/service/ProductComparisonService.java
+++ src/main/java/com/marsol/sync/domain/service/ProductComparisonService.java
@@ -1,20 +1,10 @@
 package com.marsol.sync.domain.service;
 
-import com.marsol.sync.domain.model.Scale;
 import com.marsol.sync.model.Item;
-import com.marsol.sync.model.structures.HeadersFilesHPRT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -26,7 +16,7 @@ public class ProductComparisonService {
     public List<Integer> compareProducts(List<Item> serverProducts, List<Integer> scaleProducts){
         try{
             if(serverProducts == null || scaleProducts == null){
-                throw new IllegalArgumentException("Las listas no pueden ser nulas.");
+                throw new IllegalArgumentException("Las listas serverProducts | scaleProducts no pueden ser nulas.");
             }
             Set<Integer> serverPLU_codes = serverProducts.stream()
                     .map(item -> {
@@ -41,9 +31,7 @@ public class ProductComparisonService {
                     .toList();
 
         } catch (Exception e) {
-            //System.err.println("Error al obtener productos a eliminar: "+e.getMessage());
             logger.error("Error al obtener productos a eliminar: {}",e.getMessage());
-            e.printStackTrace();
             return Collections.emptyList();
         }
     }
