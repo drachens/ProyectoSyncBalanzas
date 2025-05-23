b59c886b931feca1f195bba5d9bebefbe1ef118e
drachens <drachen_665@hotmail.com>
2025-05-23 15:41:29 -0400

    [22/05/2025] feature: Se desarrollan los test de integración para clase DeleteScaleProductService

--- src/test/java/integration/DeleteScaleProductServiceTest.java
+++ src/test/java/integration/DeleteScaleProductServiceTest.java
@@ -1,46 +1,148 @@
 package integration;
 
-import com.marsol.sync.MainClass;
 import com.marsol.sync.domain.model.Scale;
 import com.marsol.sync.domain.service.*;
+import com.marsol.sync.infraestructure.integration.SyncDataLoader;
+import com.marsol.sync.model.Item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = MainClass.class)
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 public class DeleteScaleProductServiceTest {
 
-    @Autowired
-    private DataExtractionService dataExtractionService;
+    @Configuration
+    static class ContextConfiguration {
+        @Bean
+        public SyncDataLoader syncDataLoader() {return new SyncDataLoader();}
 
+        @Bean
+        public DeleteScaleProductService deleteScaleProductService() {return new DeleteScaleProductService(syncDataLoader());}
+
+        @Bean
+        public ScaleDataReaderService scaleDataReaderService() {return new ScaleDataReaderService();}
+
+        @Bean
+        public WriteDeleteFileService writeDeleteFileService() {return new WriteDeleteFileService();}
+
+        @Bean
+        public ProductComparisonService productComparisonService() {return new ProductComparisonService();}
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(DeleteScaleProductServiceTest.class);
+    @Autowired
+    private DeleteScaleProductService service;
+    @Autowired
+    private ScaleDataReaderService scaleDataReaderService;
+    @Autowired
+    private WriteDeleteFileService writeDeleteFileService;
     @Autowired
     private ProductComparisonService productComparisonService;
 
-    @Autowired
-    private ScaleDataReaderService scaleDataReaderService;
+    private Scale scale,wrongScale;
+    private List<Item> serverProducts;
 
-    @Autowired
-    private WriteDeleteFileService writeDeleteFileService;
-
-    @Autowired
-    private DeleteScaleProductService service;
-
-    private Scale scale;
 
     @BeforeEach
     void setUp() throws Exception {
+        /**
+         * BALANZAS
+         */
         scale = Scale.builder()
                 .store(72)
-                .departamento(94)
-                .iP_Balanza("10.107.127.109")
+                .departamento(80)
+                .IP_Balanza("192.168.2.52")
                 .build();
+        wrongScale = Scale.builder()
+                .IP_Balanza("10.0.0.1")
+                .store(72)
+                .departamento(80)
+                .build();
+
+        /**
+         * ITEMS
+         */
+        Item item1 = Item.builder()
+                .plu_nbr(6044)
+                .build();
+        Item item2 = Item.builder()
+                .plu_nbr(6075)
+                .build();
+        Item item3 = Item.builder()
+                .plu_nbr(6077)
+                .build();
+        Item item4 = Item.builder()
+                .plu_nbr(6080)
+                .build();
+
+        serverProducts = List.of(item1, item2, item3, item4);
     }
 
     @Test
-    void test_procesoCompletoDeEliminadoExitoso(){
+    void test_procesoCompletoDeEliminadoExitoso() throws Exception {
+        List<Integer> scaleProducts;
+        try{
+            scaleProducts = scaleDataReaderService.getProductFromScale(scale);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            scaleProducts = Collections.emptyList();
+        }
+        List<Integer> productsToDelete;
+        try{
+            productsToDelete = productComparisonService.compareProducts(serverProducts, scaleProducts);
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            productsToDelete = Collections.emptyList();
+        }
+        try{
+            writeDeleteFileService.generateDeleteFile(productsToDelete,scale);
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+        }
 
+        try{
+            service.deleteFromScale(scale);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    @Test
+    void test_eliminacionABalanzaInexistente(){
+        List<Integer> scaleProducts;
+        try{
+            scaleProducts = scaleDataReaderService.getProductFromScale(wrongScale);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            scaleProducts = Collections.emptyList();
+        }
+        List<Integer> productsToDelete;
+        try{
+            productsToDelete = productComparisonService.compareProducts(serverProducts, scaleProducts);
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            productsToDelete = Collections.emptyList();
+        }
+        try{
+            writeDeleteFileService.generateDeleteFile(productsToDelete,wrongScale);
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        try{
+            service.deleteFromScale(scale);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
     }
 }
