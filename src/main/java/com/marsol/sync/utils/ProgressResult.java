ee8228919fdaf9f3b7d0a2e9ddab579afe3c51d4
drachens <drachen_665@hotmail.com>
2025-05-23 15:52:00 -0400

    [22/05/2025] feature: Se implementa clase ProgressResult para asignar un estado successful o no a una carga/descarga de datos a la balanza

---
+++ src/main/java/com/marsol/sync/utils/ProgressResult.java
@@ -0,0 +1,12 @@
+package com.marsol.sync.utils;
+
+public class ProgressResult {
+    private boolean successful = true;
+
+    public boolean isSuccessful() {
+        return successful;
+    }
+    public void setSuccessful(boolean successful) {
+        this.successful = successful;
+    }
+}
